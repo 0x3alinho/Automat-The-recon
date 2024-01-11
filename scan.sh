58057cfb6d4a199128d0642f2e687d95bb09a8a8
Ali Karam <106202881+0x3alinho@users.noreply.github.com>
2024-01-11 06:24:29 +0200

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -1,30 +1,19 @@
-
         # host discoery
-input_file=cidr.txt
+#input_file=cidr.txt
 
-while IFS= read -r cidr; do
-    nmap -n -Pn -sS "$cidr" | grep "for" | cut -d " " -f 5 >> IP.txt
-done < "$input_file"
+#while IFS= read -r cidr; do
+ #   nmap -n -Pn -sS --top-ports 100 "$cidr" | grep "for" | cut -d " " -f 5 >> IP.txt
+#done < "$input_file"
 ############################################################################
 
         # get ips from domains
 # Process each domain in the input file
-input_file=all_subs.txt
-while IFS= read -r domain; do
-    # Use dig to get the IP address for the domain
-    ip_address=$(dig +short "$domain")
-
-        echo -n "$ip_address" >> IP.txt
-done < "$input_file"
-
-cat IP.txt | sort -u >> ips.txt
-rm IP.txt
+#./domians_to_ips.sh all_subs.txt ips.txt
 ###############################################################################
 
         # start the scaning wiht masscan
-masscan -p1-65535 -iL ips.txt -max-rate 10000 -oG masscan_result.txt
+masscan -p1-65535 -iL ips.txt  10000 -oG masscan_result.txt
 
-        # start scan with naabu
-naabu -iL ips.txt -p -o naabu_scan_results.txt
-
-
+        # start scan with naabut
+naabu -l ips.txt -p -o naabu_scan_results.txt
+                                                  
